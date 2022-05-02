6564b1b82a3a6391e41d30d6484f9bb64ea3b51d
AyushPun <ayushpun504@gmail.com>
2022-05-03 00:02:44 +0530

    minor minor

--- js/cricket.js
+++ js/cricket.js
@@ -44,3 +44,17 @@ setInterval(() => {
 }, 5000);
 
 
+// loader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+          "body").style.visibility = "hidden";
+        document.querySelector(
+          "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+          "#loader").style.display = "none";
+        document.querySelector(
+          "body").style.visibility = "visible";
+    }
+};
